7cd296bb5c880c080a83d1008c610a43639542fa
bvluong <bryantvinluong@gmail.com>
2017-02-25 11:46:18 -0800

    Update maze.rb

--- maze.rb
+++ maze.rb
@@ -1,64 +1,51 @@
-require 'byebug'
-
 class Maze
   attr_reader :maze
-  
+
   def initialize(maze)
     @maze = maze
     @current_position = find_start_point
     @start_position = find_start_point
     @end_position = find_end_point
     @moves_hash = Hash.new
-    @distance_hash = Hash.new
     @moves = [method(:move_up),method(:move_right),method(:move_down),method(:move_left)]
   end
-  
+
   def update_moves_hash(parent)
-    possiblemoves = Array.new
     @moves.each do |x|
-      nextmove = x.call(parent)
-      possiblemoves << nextmove if possible_move?(nextmove)
-    @moves_hash[parent] = possiblemoves
-    end
-  end
-  
-  def fill_moves_hash
-    count = 0
-    until @distance_hash.include? @end_position
-      @moves_hash.values.each do |y| 
-        y.each {|x| update_moves_hash(x) if !@moves_hash.keys.include? y}
-        y.each {|x| update_distance(x,count) }
+      if possible_move?(x.call(parent)) and !@moves_hash.include? x.call(parent)
+        @moves_hash[x.call(parent)] = parent
       end
-      count += 1
     end
   end
-  
-  def update_distance(pos,count)
-    @distance_hash[pos] = count if @distance_hash[pos] == nil
+
+  def fill_moves_hash
+    until @moves_hash.keys.include? @end_position
+      @moves_hash.keys.each {|x| update_moves_hash(x) }
+    end
   end
-  
+
   def [](pos)
     row,col = pos
     @maze[row][col]
   end
-  
+
   def []=(pos,val)
     row,col = pos
     @maze[row][col] = val
   end
-  
+
   def find_end_point
     @maze.each_index do |x|
       @maze[x].each_index { |y| return [x,y] if @maze[x][y] == "E" }
     end
   end
-  
+
   def find_start_point
     @maze.each_index do |x|
       @maze[x].each_index { |y| return [x,y] if @maze[x][y] == "S" }
     end
   end
-  
+
   def possible_move?(pos)
     self[pos] == " " or self[pos] == "E" or self[pos] == "S"
   end
@@ -66,7 +53,7 @@ class Maze
   def place_mark(pos,val)
     self[pos] = val
   end
-  
+
   def move_up(pos)
     [pos[0]-1,pos[1]]
   end
@@ -74,46 +61,37 @@ class Maze
   def move_down(pos)
     [pos[0]+1,pos[1]]
   end
-  
+
   def move_right(pos)
     [pos[0],pos[1]+1]
-  end    
+  end
 
   def move_left(pos)
     [pos[0],pos[1]-1]
   end
-  
+
   def game_over?
-    @moves.each { |x| return true if self[x.call(@start_position)] == "X" } 
+    @moves.each { |x| return true if self[x.call(@start_position)] == "X" }
     false
   end
-  
+
   def display
     y = @maze.map {|x| x.join("")}
     y.each.with_index {|a,b| p "#{b} #{a}"}
   end
-  
-  def find_shortest(arr)
-    distance = 9999
-    move = Array.new
-    arr.each {|x| move,distance = x, @distance_hash[x] if @distance_hash[x] < distance}
-    move
-  end
 
   def find_move(parent)
-    moves= Array.new
-    @moves_hash.each { |x,y| moves << x if y.include? parent} 
-    find_shortest(moves)
+    @moves_hash[parent]
   end
-  
+
   def play_turn
     nextmove = find_move(@current_position)
     place_mark(nextmove,"X")
     @current_position = nextmove
   end
-  
+
   def setup
-    update_moves_hash(@current_position)
+    @moves_hash[@current_position] = @current_position
     fill_moves_hash
     @current_position = @end_position
     until game_over?
